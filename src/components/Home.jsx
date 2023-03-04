356fd4265476c8b1514f44fe9f68d8be228c47c8
javiersanzgarcia <pracker@gmail.com>
2023-03-04 05:02:37 +0100

    Finish Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,23 +10,16 @@ const Home = () => {
         <div className="home-content">
           <h3>{t('home.hello')}</h3>
           <h2>
-            I am <span>Javier Sanz </span>
+            {t('home.iam')} <span>Javier Sanz</span>
           </h2>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae,
-            mollitia alias amet earum, obcaecati numquam nesciunt vero
-            reprehenderit veniam, quas repellat in ratione.
-          </p>
+          <p> {t('home.resume')}</p>
         </div>
 
         <div className="social">
-          <a href="#">
-            <i className="fa fa-twitter"></i>
-          </a>
-          <a href="#">
+          <a href={t('home.linkedin')} target="_blank">
             <i className="fa fa-linkedin"></i>
           </a>
-          <a href="#">
+          <a href="https://github.com/javiersanzgarcia" target="_blank">
             <i className="fa fa-github"></i>
           </a>
         </div>
