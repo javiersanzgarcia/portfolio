e64d290645bce8763e47795d3d71d196c91e11d7
javiersanzgarcia <pracker@gmail.com>
2023-03-20 21:09:51 +0100

    Whole about section with translations, responsive design and Update CV

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
 import '../css/Resume.css'
 import Aos from 'aos'
 import 'aos/dist/aos.css'
 
 const Resume = () => {
+  const { t } = useTranslation('global')
+  const jobs = t('resume.jobs', { returnObjects: true })
+
   useEffect(() => {
     Aos.init({ duration: 1500, once: true, easing: 'ease' })
   }, [])
@@ -11,64 +15,40 @@ const Resume = () => {
     <>
       <section className="resume">
         <div className="main-title">
-          <h2>My Resume</h2>
+          <h2>{t('resume.title')}</h2>
         </div>
 
         <div className="resume-row">
           <div className="res-col">
             <div className="timeline">
               <ul>
-                <li>
-                  <div data-aos="fade-right" className="content">
-                    <span className="date">2017 - 2023</span>
-                    <h4>Freelancer</h4>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusm tempor incididunt ut labore et dolore magna
-                      aliqua Lorem ipsum dolor sit amet, consectetur adipisicing
-                      elit.
-                    </p>
-                  </div>
-                </li>
-
-                <li>
-                  <div data-aos="fade-left" className="content">
-                    <span className="date">2018 - 2023</span>
-                    <h4>Developer</h4>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusm tempor incididunt ut labore et dolore magna
-                      aliqua Lorem ipsum dolor sit amet, consectetur adipisicing
-                      elit.
-                    </p>
-                  </div>
-                </li>
-
-                <li>
-                  <div data-aos="fade-right" className="content">
-                    <span className="date">2015 - 2023</span>
-                    <h4>Designer</h4>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusm tempor incididunt ut labore et dolore magna
-                      aliqua Lorem ipsum dolor sit amet, consectetur adipisicing
-                      elit.
-                    </p>
-                  </div>
-                </li>
-
-                <li>
-                  <div data-aos="fade-left" className="content">
-                    <span className="date">2014 - 2023</span>
-                    <h4>Seo Expert</h4>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusm tempor incididunt ut labore et dolore magna
-                      aliqua Lorem ipsum dolor sit amet, consectetur adipisicing
-                      elit.
-                    </p>
-                  </div>
-                </li>
+                {jobs.map((job, index) =>
+                  index % 2 === 0 ? (
+                    <li key={index}>
+                      <div data-aos="fade-right" className="content">
+                        <span className="date">{job.timeJob}</span>
+                        <h4>{job.position}</h4>
+                        {job.skills.map((skill, index) => (
+                          <span key={index} className="textLeft">
+                            {skill}
+                          </span>
+                        ))}
+                      </div>
+                    </li>
+                  ) : (
+                    <li key={index}>
+                      <div data-aos="fade-left" className="content">
+                        <span className="date">{job.timeJob}</span>
+                        <h4>{job.position}</h4>
+                        {job.skills.map((skill, index) => (
+                          <span key={index} className="textLeft">
+                            {skill}
+                          </span>
+                        ))}
+                      </div>
+                    </li>
+                  )
+                )}
               </ul>
             </div>
           </div>
