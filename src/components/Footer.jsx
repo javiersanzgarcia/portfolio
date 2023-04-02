39237ffce0a977849916a95de3a8a15fe397239c
javiersanzgarcia <pracker@gmail.com>
2023-04-02 21:02:37 +0200

    Completing Footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,16 @@
 import React, { Fragment } from 'react'
+import { useTranslation } from 'react-i18next'
 import '../css/Footer.css'
 
 const Footer = () => {
+  const { t } = useTranslation('global')
+
   return (
     <Fragment>
       <section className="footer">
         <div className="footer-container">
-          <p>
-            Copyright ©2023 All rights reserved by Javier Sanz Garc&iacute;a
-          </p>
+          <p>Copyright ©2023</p>
+          <p>{t('footer.rights')} Javier Sanz Garc&iacute;a </p>
         </div>
       </section>
     </Fragment>
