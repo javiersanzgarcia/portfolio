853c8ffb9181cf93b93854a3401e13dedd3988d4
Javier Sanz García <pracker@gmail.com>
2023-04-23 21:47:50 +0200

    Last commit of this portfolio development

--- src/utils/images.js
+++ src/utils/images.js
@@ -2,8 +2,17 @@ import nVim from '../assets/Images/Nvim.jpg'
 import liferayTraining from '../assets/Images/Liferay_Training.jpg'
 import portfolio from '../assets/Images/Portfolio.jpg'
 import travelAgency from '../assets/Images/Travel_Agency.jpg'
+import monorepo from '../assets/Images/Monorepo.jpg'
+import mernDocker from '../assets/Images/Mern_Docker.jpg'
 
-const images = [nVim, liferayTraining, portfolio, travelAgency]
+const images = [
+  nVim,
+  liferayTraining,
+  portfolio,
+  travelAgency,
+  monorepo,
+  mernDocker
+]
 
 export const getImgURL = (img) => {
   return images.find((image) => image.includes(img))
